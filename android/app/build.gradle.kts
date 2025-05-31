601ffa069671c65ebe5cfd989569e9c2773c9c57
JakeNLelis <jakelelis05@gmail.com>
2025-05-31 15:33:00 +0800

    Changing the release key for Android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.game_hub"
+    namespace = "com.nietzchan.game_hub"
     ndkVersion = "25.1.8937393"
     compileSdk = 35
 
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.game_hub"
+        applicationId = "com.nietzchan.game_hub"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
